8016a546949ebad98420155e1b5b43463e0c9357
Harshit Pachori <harshitpachori345@gmail.com>
2024-07-22 11:32:55 +0530

    Bug : fixed a bug related to Zustand in SocketContext

--- cleartalk_frontend/src/context/SocketContext.jsx
+++ cleartalk_frontend/src/context/SocketContext.jsx
@@ -21,6 +21,21 @@ export const SocketProvider = ({ children }) => {
       socket.current.on("connect", () => {
         console.log("Connected to socket server");
       });
+
+      const handleRecieveMessage = (message) => {
+        const { selectedChatData, selectedChatType, addMessage } =
+          useAppStore.getState();
+        if (
+          (selectedChatType !== undefined &&
+            selectedChatData._id === message.sender._id) ||
+          selectedChatData._id === message.recipient._id
+        ) {
+          addMessage(message);
+        }
+      };
+
+      socket.current.on("recieveMessage", handleRecieveMessage);
+
       return () => {
         socket.current.disconnect();
       };
