2c2e5d54f370c0f112efaaa5e9fe9eb0a70eea18
Harshit Pachori <harshitpachori345@gmail.com>
2024-07-29 16:48:50 +0530

    Bug : fixed a bug in auth controller for sameSite cookie for cross origin

--- cleartalk_backend/controllers/AuthController.js
+++ cleartalk_backend/controllers/AuthController.js
@@ -61,6 +61,7 @@ export const login = async (req, res, next) => {
       expires: cookieValidity,
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
+      sameSite:"none"
     });
     return res.status(200).json({
       user: {
